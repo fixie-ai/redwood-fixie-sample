232923ccb1afe4cd81e67ad8247a5488e9e1c02e
Nick Heiner <nick.heiner+github@gmail.com>
2023-09-23 09:04:41 -0700

    Add controlled example

--- web/src/pages/ControlledFloatingEmbedPage/ControlledFloatingEmbedPage.tsx
+++ web/src/pages/ControlledFloatingEmbedPage/ControlledFloatingEmbedPage.tsx
@@ -1,7 +1,22 @@
-import { Link, routes } from '@redwoodjs/router'
 import { MetaTags } from '@redwoodjs/web'
+import { ControlledFloatingFixieEmbed } from 'fixie/web'
+import { useState } from 'react'
 
 const ControlledFloatingEmbedPage = () => {
+  const [visible, setVisible] = useState(false)
+
+  const style = {
+    position: 'fixed',
+    top: '50%',
+    left: '50%',
+    transform: 'translate(-50%, -50%)',
+    zIndex: 1000,
+    width: '80%',
+    height: '80%',
+    overflow: 'auto',
+    backgroundColor: 'rgba(0,0,0,0.4)',
+  }
+
   return (
     <>
       <MetaTags
@@ -10,20 +25,11 @@ const ControlledFloatingEmbedPage = () => {
       />
 
       <h1>ControlledFloatingEmbedPage</h1>
-      <p>
-        Find me in{' '}
-        <code>
-          ./web/src/pages/ControlledFloatingEmbedPage/ControlledFloatingEmbedPage.tsx
-        </code>
-      </p>
-      <p>
-        My default route is named <code>controlledFloatingEmbed</code>, link to
-        me with `
-        <Link to={routes.controlledFloatingEmbed()}>
-          ControlledFloatingEmbed
-        </Link>
-        `
-      </p>
+      <p>This page embeds the off-the-shelf Fixie Agent UI. Open and close it via the <span className='code'>visible</span> prop.</p>
+
+      <button onClick={() => setVisible(prev => !prev)}>{visible ? 'Hide' : 'Show'}</button>
+
+      <ControlledFloatingFixieEmbed visible={visible} debug agentId='nick/sidekick-help-scout' style={style} />
     </>
   )
 }
